ba534710e815eeaa2e3b96bd408d0e7b588b5c6d
Daniyel19084 <124051408@upq.edu.mx>
2025-09-09 16:05:19 -0600

    JSprincipiantes

---
+++ PRUEBA/app.js
@@ -0,0 +1,30 @@
+//cadena/string
+"hola mundo"
+
+//nunmero
+19084 
+
+//booleanos
+true false
+
+//arreglos
+["ddd","ddd"];
+
+//objeto
+"dias" 365;
+"personas" 12;
+"calificacion" 65.6;
+
+//variable(var) y let son similares, para crear variables
+var nombre = "Daniyel";
+let user = "Daniyel";
+let num;
+num = 84;
+
+//con el comando "const" la variable sera fija
+const PI = 3.14;
+
+//el console.log() sera el print
+console.log("hola");
+document.write("Hola")
+//con el document.write es para que aparezca el texto en la pagina
