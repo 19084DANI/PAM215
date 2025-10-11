f066a88a08bca6631791be6431d920674e2c934e
Daniyel19084 <124051408@upq.edu.mx>
2025-10-10 20:08:38 -0600

    cambios realizados10/1025. creacion de app de incrementar, disminuir y reinicar

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,28 @@
+/*1-.import: zona de importaciones
+  todo lo que egregaremos a nuestra pantalla en especifico*/
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Button } from 'react-native';
+import React,{useState} from 'react';
 
+//2-. mainScreen:zona de comoponentes
 export default function App() {
+
+  const[Contador,setcontaddor]=useState(0);//destructuracion
+
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo en React Native!</Text>
+
+      <Text>Contador: {Contador} </Text>
+      <Button title='Incrementar'onPress={()=>setcontaddor(Contador+1)}></Button>
+      <Button title='Quitar'onPress={()=>setcontaddor(Contador-1)}></Button> 
+      <Button title='Reiniciar'onPress={()=>setcontaddor(Contador-Contador)}></Button>          
       <StatusBar style="auto" />
+
     </View>
   );
-}
 
+}
+//3-.Style: zona de estilos y posicionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,
